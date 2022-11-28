8fa3975720fa5fdf680534fe33ef1e1cb88a61b0
jahidhasan234031 <jahidhasan234031@mail.com>
2022-11-28 19:53:18 +0600

    jwt api done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const colors = require('colors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
@@ -18,6 +19,25 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6l0by.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    // console.log('token vvv', req.headers.authorization)
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        return res.status(401).send('unauthorize access')
+    }
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access1' })
+        }
+        req.decoded = decoded
+        next()
+    })
+
+}
 
 
 async function run() {
@@ -51,19 +71,33 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/bikeBooking', async (req, res) => {
-            // const query = {}
-            // console.log('token', req.headers.authorization)
+        app.get('/bikeBooking', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            // const decodedEmail = req.decoded.email;
-            // console.log(decodedEmail)
-            // if (email !== decodedEmail) {
-            //     return res.status(403).send({ message: 'forbidden access2' })
-            // }
+            const decodedEmail = req.decoded.email;
+            console.log(decodedEmail)
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access2' })
+            }
             const query = { email: email }
             const bookings = await bikeBookings.find(query).toArray()
             res.send(bookings)
 
+        })
+
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const user = await usersCollections.findOne(query)
+            // console.log(user)
+            // res.send({ accessToken: "token" })
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '12h' });
+                // console.log(process.env.ACCESS_TOKEN)
+                return res.send({ accessToken: token })
+            }
+
+            res.status(403).send({ accessToken: '' })
         })
 
 
@@ -72,6 +106,8 @@ async function run() {
             const result = await usersCollections.insertOne(user)
             res.send(result)
         })
+
+
 
 
     }
